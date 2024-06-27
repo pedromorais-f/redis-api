b1b9f07bef2098532b52f6e8b85e7a9a82aede20
Pedro <contatopdrm@gmail.com>
2024-06-27 19:25:37 -0300

    add publisher and subscriber

--- pub-sub test/publishertest.py
+++
@@ -1,7 +0,0 @@
-import redis
-
-cliente_redis = redis.StrictRedis(host="localhost",port=6379,db=0)
-canal = "nicolas gameplays"
-while True:
-    mensagem = input("Enter a message: ")
-    cliente_redis.publish(canal,mensagem)
