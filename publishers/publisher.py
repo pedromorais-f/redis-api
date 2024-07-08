68546f6953e5d5685ddc5f85e10e90b5c2562fd6
Pedro <contatopdrm@gmail.com>
2024-07-08 19:06:14 -0300

    update publisher.py

--- publishers/publisher.py
+++ publishers/publisher.py
@@ -1,12 +1,11 @@
 import redis
 import cv2
-import sys
 
 redis_client = redis.Redis(host="localhost",port=6379)
 channel = "MNIST IMAGES"
 
 while True:
     image_file = input("Image:")
-    message = cv2.imread(f"../publishers/inputs/{image_file}")
+    message = cv2.imread(f"publishers/inputs/5.jpg")
     message = cv2.imencode('.jpg', message)[1].tobytes()
     redis_client.publish(channel, message)
