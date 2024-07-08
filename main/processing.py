9e2d6b80d6562a83507c15264fd930306aec9450
Pedro <contatopdrm@gmail.com>
2024-07-08 18:54:41 -0300

    update Dockerfile and compose and update api

--- main/processing.py
+++ main/processing.py
@@ -1,31 +1,25 @@
 import redis
 import cv2
 import numpy as np
-import logging
+from utils.pubsub_utils import get_module_logger
 
-def get_module_logger(mod_name):
-
-    logger = logging.getLogger(mod_name)
-    handler = logging.StreamHandler()
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
-
-cliente_redis = redis.Redis(host="redis",port=6379)
+redis_client = redis.Redis(host="redis",port=6379, db=0)
 
 channels = ["MNIST IMAGES", "Prediction"]
 
-pubsub = cliente_redis.pubsub()
+pubsub = redis_client.pubsub()
 pubsub.subscribe(channels[0])
 
-get_module_logger(__name__).info(f"inscrito no canal {channels[0]}. aguardando filosofias...")
+logger = get_module_logger()
+
+logger.info(f"inscrito no canal {channels[0]}. aguardando filosofias...")
 
 for message in pubsub.listen():
-    get_module_logger(__name__).info(message)
+    logger.info(message)
 
     if message['type'] == 'message':
         image = message['data']
         imagem_decode = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
 
-        cliente_redis.publish(channels[1], "Testing")
-        get_module_logger(__name__).info("Prediction published")
+        redis_client.publish(channels[1], "Testing")
+        logger.info("Prediction published")
--- subscribers/subscriber.py
+++ subscribers/subscriber.py
@@ -1,14 +1,5 @@
 import redis
-import logging
-
-
-def get_module_logger(mod_name):
-
-    logger = logging.getLogger(mod_name)
-    handler = logging.StreamHandler()
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
+from utils.pubsub_utils import get_module_logger
 
 redis_client = redis.Redis(host="redis",port=6379)
 channel = 'Prediction'
@@ -16,10 +7,12 @@ channel = 'Prediction'
 pubsub = redis_client.pubsub()
 pubsub.subscribe(channel)
 
-get_module_logger(__name__).info(f"Subscribed in {channel} Channel")
+logger = get_module_logger()
+
+logger.info(f"Subscribed in {channel} Channel")
 
 for message in pubsub.listen():
-    get_module_logger(__name__).info(message)
+    logger.info(message)
     if message['type'] == 'message':
         str = message['data']
-        get_module_logger(__name__).info(str)
+        logger.info(str)
